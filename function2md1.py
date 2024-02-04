f25dfc510b186283db18108d97cd98a7119670b8
SomeoneSlicedMyEyes <t_jumashev@kbtu.kz>
2024-02-04 12:25:30 +0600

    function2md1

---
+++ function2md1.py
@@ -0,0 +1,202 @@
+#1
+def is_highly_rated(movie):
+    return movie["imdb"] > 5.5
+
+example = {
+    "name": "Usual Suspects",
+    "imdb": 7.0,
+    "category": "Thriller"
+}
+result = is_highly_rated(example)
+print(result)
+#2
+def print_highly_rated_movies(movie_list):
+    for movie in movie_list:
+        if movie["imdb"] > 5.5:
+            print(movie["name"])
+movies = [
+    {"name": "Usual Suspects", "imdb": 7.0, "category": "Thriller"},
+    {"name": "Hitman", "imdb": 6.3, "category": "Action"},
+    {"name": "Dark Knight", "imdb": 9.0, "category": "Adventure"},
+    {"name": "The Help", "imdb": 8.0, "category": "Drama"},
+    {"name": "The Choice", "imdb": 6.2, "category": "Romance"},
+    {"name": "Colonia", "imdb": 7.4, "category": "Romance"},
+    {"name": "Love", "imdb": 6.0, "category": "Romance"},
+    {"name": "Bride Wars", "imdb": 5.4, "category": "Romance"},
+    {"name": "AlphaJet", "imdb": 3.2, "category": "War"},
+    {"name": "Ringing Crime", "imdb": 4.0, "category": "Crime"},
+    {"name": "Joking muck", "imdb": 7.2, "category": "Comedy"},
+    {"name": "What is the name", "imdb": 9.2, "category": "Suspense"},
+    {"name": "Detective", "imdb": 7.0, "category": "Suspense"},
+    {"name": "Exam", "imdb": 4.2, "category": "Thriller"},
+    {"name": "We Two", "imdb": 7.2, "category": "Romance"}
+]
+print("Movies with an IMDB score above 5.5:")
+print_highly_rated_movies(movies)
+
+#3
+def get_movies_by_category(movie_list, category):
+    return [movie for movie in movie_list if movie["category"] == category]
+movies = [
+    {"name": "Usual Suspects", "imdb": 7.0, "category": "Thriller"},
+    {"name": "Hitman", "imdb": 6.3, "category": "Action"},
+    {"name": "Dark Knight", "imdb": 9.0, "category": "Adventure"},
+    {"name": "The Help", "imdb": 8.0, "category": "Drama"},
+    {"name": "The Choice", "imdb": 6.2, "category": "Romance"},
+    {"name": "Colonia", "imdb": 7.4, "category": "Romance"},
+    {"name": "Love", "imdb": 6.0, "category": "Romance"},
+    {"name": "Bride Wars", "imdb": 5.4, "category": "Romance"},
+    {"name": "AlphaJet", "imdb": 3.2, "category": "War"},
+    {"name": "Ringing Crime", "imdb": 4.0, "category": "Crime"},
+    {"name": "Joking muck", "imdb": 7.2, "category": "Comedy"},
+    {"name": "What is the name", "imdb": 9.2, "category": "Suspense"},
+    {"name": "Detective", "imdb": 7.0, "category": "Suspense"},
+    {"name": "Exam", "imdb": 4.2, "category": "Thriller"},
+    {"name": "We Two", "imdb": 7.2, "category": "Romance"}
+]
+
+category = "Romance"
+romance_movies = get_movies_by_category(movies, category)
+print(f"Movies under the category '{category}':")
+for movie in romance_movies:
+    print(movie["name"])
+
+#4
+def average_imdb(movie_list):
+    if not movie_list:
+        return 0.0
+
+    total = sum(movie["imdb"] for movie in movie_list)
+    average = total / len(movie_list)
+    return average
+movies = [
+    {"name": "Usual Suspects", "imdb": 7.0, "category": "Thriller"},
+    {"name": "Hitman", "imdb": 6.3, "category": "Action"},
+    {"name": "Dark Knight", "imdb": 9.0, "category": "Adventure"},
+    {"name": "The Help", "imdb": 8.0, "category": "Drama"},
+    {"name": "The Choice", "imdb": 6.2, "category": "Romance"},
+    {"name": "Colonia", "imdb": 7.4, "category": "Romance"},
+    {"name": "Love", "imdb": 6.0, "category": "Romance"},
+    {"name": "Bride Wars", "imdb": 5.4, "category": "Romance"},
+    {"name": "AlphaJet", "imdb": 3.2, "category": "War"},
+    {"name": "Ringing Crime", "imdb": 4.0, "category": "Crime"},
+    {"name": "Joking muck", "imdb": 7.2, "category": "Comedy"},
+    {"name": "What is the name", "imdb": 9.2, "category": "Suspense"},
+    {"name": "Detective", "imdb": 7.0, "category": "Suspense"},
+    {"name": "Exam", "imdb": 4.2, "category": "Thriller"},
+    {"name": "We Two", "imdb": 7.2, "category": "Romance"}
+]
+
+average = average_imdb(movies)
+print(f"Average IMDB score of the movies: {average:.2f}")
+
+#5
+def average_imdb_by_category(movie_list, category):
+    movies = [movie for movie in movie_list if movie["category"] == category]
+
+    if not movies:
+        return 0.0
+
+    total = sum(movie["imdb"] for movie in movies)
+    average = total/ len(movies)
+    return average
+
+movies = [
+    {"name": "Usual Suspects", "imdb": 7.0, "category": "Thriller"},
+    {"name": "Hitman", "imdb": 6.3, "category": "Action"},
+    {"name": "Dark Knight", "imdb": 9.0, "category": "Adventure"},
+    {"name": "The Help", "imdb": 8.0, "category": "Drama"},
+    {"name": "The Choice", "imdb": 6.2, "category": "Romance"},
+    {"name": "Colonia", "imdb": 7.4, "category": "Romance"},
+    {"name": "Love", "imdb": 6.0, "category": "Romance"},
+    {"name": "Bride Wars", "imdb": 5.4, "category": "Romance"},
+    {"name": "AlphaJet", "imdb": 3.2, "category": "War"},
+    {"name": "Ringing Crime", "imdb": 4.0, "category": "Crime"},
+    {"name": "Joking muck", "imdb": 7.2, "category": "Comedy"},
+    {"name": "What is the name", "imdb": 9.2, "category": "Suspense"},
+    {"name": "Detective", "imdb": 7.0, "category": "Suspense"},
+    {"name": "Exam", "imdb": 4.2, "category": "Thriller"},
+    {"name": "We Two", "imdb": 7.2, "category": "Romance"}
+]
+category_name = "Romance"
+average_imdb_in_category = average_imdb_by_category(movies, category_name)
+print(f"Average IMDB score of {category_name} movies: {average_imdb_in_category:.2f}")
+    
+
+movies = [
+{
+"name": "Usual Suspects", 
+"imdb": 7.0,
+"category": "Thriller"
+},
+{
+"name": "Hitman",
+"imdb": 6.3,
+"category": "Action"
+},
+{
+"name": "Dark Knight",
+"imdb": 9.0,
+"category": "Adventure"
+},
+{
+"name": "The Help",
+"imdb": 8.0,
+"category": "Drama"
+},
+{
+"name": "The Choice",
+"imdb": 6.2,
+"category": "Romance"
+},
+{
+"name": "Colonia",
+"imdb": 7.4,
+"category": "Romance"
+},
+{
+"name": "Love",
+"imdb": 6.0,
+"category": "Romance"
+},
+{
+"name": "Bride Wars",
+"imdb": 5.4,
+"category": "Romance"
+},
+{
+"name": "AlphaJet",
+"imdb": 3.2,
+"category": "War"
+},
+{
+"name": "Ringing Crime",
+"imdb": 4.0,
+"category": "Crime"
+},
+{
+"name": "Joking muck",
+"imdb": 7.2,
+"category": "Comedy"
+},
+{
+"name": "What is the name",
+"imdb": 9.2,
+"category": "Suspense"
+},
+{
+"name": "Detective",
+"imdb": 7.0,
+"category": "Suspense"
+},
+{
+"name": "Exam",
+"imdb": 4.2,
+"category": "Thriller"
+},
+{
+"name": "We Two",
+"imdb": 7.2,
+"category": "Romance"
+}
+]
